fdfd3ae59b816c79c2fc5b7612fd1ff2940269ba
ayequill <ayequillgh@gmail.com>
2023-02-23 12:44:19 +0000

    use aos cdn for css

--- src/components/ProjectInMind.jsx
+++ src/components/ProjectInMind.jsx
@@ -1,30 +1,14 @@
-import { useEffect } from "react";
 import project from "../assets/14.png";
-import {projectTextAnimate} from "../hooks/animations";
 
 export default function ProjectInMind() {
-  useEffect(() => projectTextAnimate, []);
-
-  const gotWork = (
-    <div>
-      <h1 className="ml11">
-        <span className="text-wrapper">
-          <span className="line line1"></span>
-          <span className="letters project__title">
-            Got a new project in mind?
-          </span>
-        </span>
-      </h1>
-  </div>
-  );
 
   return (
     <>
-      <section data-aos='flip-up' data-aos-duration='1000' className="project">
+      <section data-aos='zoom-in-up' data-aos-duration='1000' className="project">
         <div className="project__bg">
           <div className="project__container container grid">
             <div className="project__data">
-              {gotWork}
+              <p className="project__title"> Got a new project in mind?</p>
               <p className="project__description">
                 Connect me now and get a 30% discount on your new project.
               </p>
